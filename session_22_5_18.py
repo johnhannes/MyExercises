8a329942b3e9ab2316687b7f5d090cde10ced52c
johnhannes <39305226+johnhannes@users.noreply.github.com>
2018-05-29 15:31:18 +0200

    Add files via upload

---
+++ session_22_5_18.py
@@ -0,0 +1,6 @@
+>>> def myFunction(verein,tabellenplatz,gründungsjahr):
+...     name = verein + " " + tabellenplatz + " " + gründungsjahr
+...     print(name)
+...
+>>> myFunction("werder_bremen","11","1899")
+werder_bremen 11 1899
